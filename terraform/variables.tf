ac8e3b239803ac39acc02c62065ba6ab4f7ea5ca
abdelrahmanMsaeed <eng.abdelrahman.M.saeed@gmail.com>
2022-02-01 16:33:26 +0200

    added variables file.

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "region" {
+    type = string
+}
+
+variable "project_name" {
+    type = string
+}
+
+variable "public_subnet_name" {
+    type = string
+}
+
+variable "private_subnet_name" {
+    type = string
+}
+variable "project_id" {
+    type = string
+}
+
+variable "network_name" {
+    type = string
+}
+
+variable "router_name" {
+    type = string
+}
+
+variable "public_cidr" {
+    type = string
+}
+
+variable "private_cidr" {
+    type = string
+}
