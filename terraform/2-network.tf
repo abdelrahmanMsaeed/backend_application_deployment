e57d360e4f9ba861459c61a01851000e9a4e1b4e
abdelrahmanMsaeed <eng.abdelrahman.M.saeed@gmail.com>
2022-02-01 16:30:42 +0200

    added network and subnets

---
+++ terraform/2-network.tf
@@ -0,0 +1,40 @@
+resource "google_compute_network" "vpc_network" {
+  name = var.network_name
+  auto_create_subnetworks = false
+}
+
+
+# public subnet
+resource "google_compute_subnetwork" "public_subnet" {
+    name = var.public_subnet_name
+    ip_cidr_range = var.public_cidr
+    region = var.region
+    network = var.network_name
+    depends_on = [google_compute_network.vpc_network]
+}
+
+
+# private subnet
+resource "google_compute_subnetwork" "private_subnet" {
+    name = var.private_subnet_name
+    ip_cidr_range = var.private_cidr
+    region = var.region
+    network = var.network_name
+    depends_on = [google_compute_network.vpc_network]
+}
+
+
+# firewall
+resource "google_compute_firewall" "rules" {
+  project     = var.project_name
+  name        = "allowtcprules"
+  network     = var.network_name
+  depends_on = [google_compute_network.vpc_network]
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["80", "22", "443"]
+  }
+  
+  source_ranges = ["35.235.240.0/20"]
+}
